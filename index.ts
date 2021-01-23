0254d7a9f424e80f1ae6fa2ea243bc1ae0b6cf9c
2banic <bergesebastian@gmail.com>
2021-01-23 15:51:12 +0100

    erster log test

--- index.ts
+++ index.ts
@@ -1,6 +1,10 @@
 // Import stylesheets
-import './style.css';
+import "./style.css";
 
 // Write TypeScript code!
-const appDiv: HTMLElement = document.getElementById('app');
+const appDiv: HTMLElement = document.getElementById("app");
 appDiv.innerHTML = `<h1>TypeScript Starter action</h1>`;
+
+var number = 1;
+let count =2;
+console.log("hallo", count + number);
